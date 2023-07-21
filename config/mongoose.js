0ba0bf1310a6b0850dc15b7eae8be7de2bc948f9
prachigupta921 <gprachi921@gmail.com>
2023-07-21 22:13:47 +0530

    ch

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.set("strictQuery", false);
 
-mongoose.connect("mongodb://127.0.0.1:27017/Auth")
+mongoose.connect("mongodb+srv://Prachi_056:%40Prac056@realmcluster.8e9iw.mongodb.net/Auth-Node-js")
   .then(() => console.log('Connected to Database'))
   .catch(err => console.log('Error connecting to database', err));
+
